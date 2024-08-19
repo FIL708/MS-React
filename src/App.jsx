f7443a6e0f7b5de24e4d4d54ffb0916ac83eed14
FIL708 <filip.zebrowski4013@gmail.com>
2024-08-19 12:00:43 +0200

    split components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import CoreConcept from './components/CoreConcept/CoreConcept';
+import ConceptsSection from './components/ConceptsSection/ConceptsSection';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
-import { CORE_CONCEPTS } from './data';
 import './styles.css';
 
 function App() {
     return (
-        <div>
+        <>
             <Header />
             <main>
-                <section id='core-concepts'>
-                    <h2>Core Concepts</h2>
-                    <ul>
-                        {CORE_CONCEPTS.map((concept) => (
-                            <CoreConcept {...concept} key={concept.title} />
-                        ))}
-                    </ul>
-                </section>
+                <ConceptsSection />
                 <Menu />
             </main>
-        </div>
+        </>
     );
 }
 
